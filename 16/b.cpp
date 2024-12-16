98245369ace66cb1364d03f01ac42fbf89abeafc
Dan Shved <danshved@gmail.com>
2024-12-16 07:28:55 +0100

    Add solutions for day 16

---
+++ 16/b.cpp
@@ -0,0 +1,135 @@
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <limits>
+#include <map>
+#include <optional>
+#include <ranges>
+#include <set>
+#include <string>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+#include "collections.h"
+#include "numbers.h"
+#include "order.h"
+#include "parse.h"
+
+struct Coord {
+    int i;
+    int j;
+
+    Coord operator+(const Coord& other) {
+        return {i + other.i, j + other.j};
+    }
+};
+
+const Coord kDirs[4] = {{0, 1}, {-1, 0}, {0, -1}, {1, 0}};
+enum { kEast, kNorth, kWest, kSouth };
+
+const int kInf = std::numeric_limits<int>::max();
+
+template <typename T>
+using VVV = std::vector<std::vector<std::vector<T>>>;
+
+template <typename T>
+VVV<T> MakeVVV(int size1, int size2, int size3, const T& val) {
+    return VVV<T>(size1, std::vector<std::vector<T>>(
+                             size2, std::vector<T>(size3, val)));
+}
+
+VVV<int> DistancesFrom(const std::vector<std::string>& input,
+                       Coord start, int start_dir) {
+    int size_i = input.size();
+    int size_j = input[0].size();
+
+    auto in_bounds = [&](const Coord& c) {
+        return c.i >= 0 && c.i < size_i && c.j >= 0 && c.j < size_j;
+    };
+
+    VVV<int> d = MakeVVV(size_i, size_j, 4, kInf);
+    VVV<bool> added = MakeVVV(size_i, size_j, 4, false);
+    d[start.i][start.j][start_dir] = 0;
+
+    while (true) {
+        // Find a node with the lowest distance that hasn't been finalized yet.
+        Coord cur;
+        int cur_dir;
+        int dist = kInf;
+        for (int i = 0; i < size_i; i++) {
+            for (int j = 0; j < size_j; j++) {
+                for (int dir = 0; dir < 4; dir++) {
+                    if (added[i][j][dir]) {
+                        continue;
+                    }
+                    if (d[i][j][dir] < dist) {
+                        dist = d[i][j][dir];
+                        std::tie(cur.i, cur.j, cur_dir) = std::make_tuple(i, j, dir);
+                    }
+                }
+            }
+        }
+        if (dist == kInf) {
+            break;
+        }
+
+        // Finalize it.
+        added[cur.i][cur.j][cur_dir] = true;
+
+        // Update distances to its 2 or 3 outgoing neighbors.
+        for (int delta = 1; delta <= 3; delta += 2) {
+            int new_dir = (cur_dir + delta) % 4;
+            int new_dist = dist + 1000;
+            if (new_dist < d[cur.i][cur.j][new_dir]) {
+                d[cur.i][cur.j][new_dir] = new_dist;
+            }
+        }
+        {
+            Coord new_c = cur + kDirs[cur_dir];
+            int new_dist = dist + 1;
+            if (in_bounds(new_c) && input[new_c.i][new_c.j] != '#' &&
+                new_dist < d[new_c.i][new_c.j][cur_dir]) {
+                d[new_c.i][new_c.j][cur_dir] = new_dist;
+            }
+        }
+    }
+
+    return d;
+}
+
+int main() {
+    std::vector<std::string> input = Split(Trim(GetContents("input.txt")), '\n');
+    int size_i = input.size();
+    int size_j = input[0].size();
+
+    Coord start, end;
+    std::tie(start.i, start.j) = FindOrDie<2>(input, 'S');
+    std::tie(end.i, end.j) = FindOrDie<2>(input, 'E');
+
+    VVV<int> from_start = DistancesFrom(input, start, kEast);
+    VVV<int> from_end = DistancesFrom(input, end, kSouth);
+
+    // Find all states lying on a shortest path. State s lies on a shortest
+    // path from b to e if and only if dist(b, e) = dist(b, s) + dist(s, e).
+    int answer = 0;
+    for (int i = 0; i < size_i; i++) {
+        for (int j = 0; j < size_j; j++) {
+            for (int dir = 0; dir < 4; dir++) {
+                if (from_start[i][j][dir] == kInf || from_end[i][j][(dir + 2) % 4] == kInf) {
+                    continue;
+                }
+                if (from_start[i][j][dir] + from_end[i][j][(dir + 2) % 4] ==
+                    from_start[end.i][end.j][kNorth]) {
+                    answer++;
+                    break;
+                }
+            }
+        }
+    }
+
+    std::cout << answer << std::endl;
+    return 0;
+}
---
+++ 16/collections.h
@@ -0,0 +1,110 @@
+#ifndef __AOC_COLLECTIONS_H__
+#define __AOC_COLLECTIONS_H__
+
+#include <cassert>
+#include <iterator>
+#include <tuple>
+#include <utility>
+#include <vector>
+
+// Transposes a matrix. Could be a std::vector<std::vector<T>>
+// or std::vector<std::string>.
+template <typename C>
+std::vector<C> Transpose(const std::vector<C>& v) {
+    int height = v.size();
+    assert(height > 0);
+    int width = v[0].size();
+    assert(width > 0);
+    for (int i = 0; i < height; i++) {
+        assert(v[i].size() == width);
+    }
+
+    std::vector<C> result;
+    result.reserve(width);
+    for (int j = 0; j < width; j++) {
+        C result_row;
+        result_row.reserve(height);
+        for (int i = 0; i < height; i++) {
+            result_row.push_back(v[i][j]);
+        }
+        result.push_back(std::move(result_row));
+    }
+    return result;
+}
+
+// Concatenates a container of containers into a single container of the same
+// type as the inner containers.
+template <typename C>
+C::value_type Concat(const C& xss) {
+    typename C::value_type result;
+    for (const auto& xs : xss) {
+        std::copy(std::begin(xs), std::end(xs), std::back_inserter(result));
+    }
+    return result;
+}
+
+// Applies a container-returning function to each element of the container and
+// concatenates the results.
+//
+// The result is the same type of container that the function returns. If it
+// returns vectors, the result is a vector. If it returns strings, a string.
+template <typename C, typename F>
+auto ConcatMap(const C& xs, F f) -> decltype(f(*std::begin(xs))) {
+    using OutputC = decltype(f(*std::begin(xs)));
+    OutputC result;
+    for (const auto& x : xs) {
+        OutputC ys = f(x);
+        std::copy(std::begin(ys), std::end(ys), std::back_inserter(result));
+    }
+    return result;
+}
+
+// Finds a specific element in a multi-dimensional container. The first
+// template parameter (n) is mandatory.
+//
+// The result is an std::optional of an n-tuple of ints. It is nullopt if
+// the element isn't found, and an actual tuple of the first occurrence if
+// it is found.
+template <size_t n, typename Cont, typename T>
+auto Find(const Cont& cont, const T& t) {
+    return FindIf<n, Cont>(cont, [&t](const T& u) { return u == t; });
+}
+
+// Like Find(), but matches a predicate.
+template <size_t n, typename Cont, typename Pred>
+auto FindIf(const Cont& cont, Pred&& pred) {
+    if constexpr (n == 0) {
+        using ResType = std::optional<std::tuple<>>;
+        return pred(cont) ? ResType(std::make_tuple()) : ResType(std::nullopt);
+    } else {
+        using ResType = decltype(std::optional(
+            std::tuple_cat(std::make_tuple((int)0),
+                           *FindIf<n - 1>(cont[0], std::forward<Pred>(pred)))));
+        for (int i = 0; i < cont.size(); i++) {
+            auto res = FindIf<n - 1>(cont[i], std::forward<Pred>(pred));
+            if (res.has_value()) {
+                return ResType(std::tuple_cat(std::make_tuple(i), *std::move(res)));
+            }
+        }
+        return ResType(std::nullopt);
+    }
+}
+
+// Convenience wrapper for Find(). Returns an n-tuple of ints, fails if the
+// element isn't found.
+template <size_t n, typename Cont, typename T>
+auto FindOrDie(const Cont& cont, const T& t) {
+    auto res = Find<n>(cont, t);
+    assert(res.has_value());
+    return *std::move(res);
+}
+// Convenience wrapper for FindIf(). Returns an n-tuple of ints, fails if the
+// element isn't found.
+template <size_t n, typename Cont, typename Pred>
+auto FindIfOrDie(const Cont& cont, Pred&& pred) {
+    auto res = FindIf<n>(cont, std::forward<Pred>(pred));
+    assert(res.has_value());
+    return *std::move(res);
+}
+
+#endif
