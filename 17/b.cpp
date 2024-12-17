6cfab56d9828ff804efb756ee7819c5df95a5e0b
Dan Shved <danshved@gmail.com>
2024-12-17 07:59:40 +0100

    Improve readability

--- 17/b.cpp
+++ 17/b.cpp
@@ -24,28 +24,29 @@ using ULL = unsigned long long;
 std::vector<ULL> program = {2,4,1,2,7,5,1,3,4,4,5,5,0,3,3,0};
 ULL answer = std::numeric_limits<ULL>::max();
 
-void solve(ULL a, ULL fixed, int shift, int pc) {
+void solve(ULL a, ULL a_fixed, int shift, int pc) {
     if (pc == program.size()) {
         answer = std::min(answer, a);
         return;
     }
     for (ULL low = 0; low < 8; low++) {
         ULL a2 = (a & ~(0x7ull << shift)) | (low << shift);
-        if ((a2 ^ a) & fixed) {
+        if ((a2 ^ a) & a_fixed) {
             continue;
         }
-        ULL new_fixed = fixed | (0x7ull << shift);
+        ULL a2_fixed = a_fixed | (0x7ull << shift);
 
         ULL f = (a2 >> shift) & 0x7ull;
-        ULL piece = program[pc] ^ f ^ 0x1ull;
-        int piece_shift = shift + (f ^ 0x2ull);
-        ULL a3 = (a2 & ~(0x7ull << piece_shift)) | (piece << piece_shift);
-        if ((a3 ^ a2) & new_fixed) {
+        ULL high = program[pc] ^ f ^ 0x1ull;
+        int high_shift = shift + (f ^ 0x2ull);
+
+        ULL a3 = (a2 & ~(0x7ull << high_shift)) | (high << high_shift);
+        if ((a3 ^ a2) & a2_fixed) {
             continue;
         }
-        new_fixed |= 0x7ull << piece_shift;
-        
-        solve(a3, new_fixed, shift + 3, pc + 1);
+        ULL a3_fixed = a2_fixed | (0x7ull << high_shift);
+
+        solve(a3, a3_fixed, shift + 3, pc + 1);
     }
 }
 
