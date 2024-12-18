5cc253ba4f46a251f53881f6b7eec7d42c40fb3b
Dan Shved <danshved@gmail.com>
2024-12-18 06:22:33 +0100

    Add solutions for day 18

---
+++ 18/b.cpp
@@ -0,0 +1,75 @@
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <limits>
+#include <map>
+#include <optional>
+#include <queue>
+#include <ranges>
+#include <set>
+#include <string>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+#include "collections.h"
+#include "numbers.h"
+#include "order.h"
+#include "parse.h"
+
+const int kSizeX = 71;
+const int kSizeY = 71;
+
+struct Coord {
+    int x, y;
+};
+
+const Coord kDirs[4] = {{1, 0}, {0, -1}, {-1, 0}, {0, 1}};
+const int kInf = std::numeric_limits<int>::max();
+
+bool InBounds(const Coord& c) {
+    return c.x >= 0 && c.x < kSizeX && c.y >= 0 && c.y < kSizeY;
+}
+
+int main() {
+    std::vector<std::string> lines = Split(Trim(GetContents("input.txt")), '\n');
+    std::vector<Coord> bytes;
+    for (const std::string& line : lines) {
+        auto [l, r] = Split2(line, ',');
+        bytes.push_back(Coord{std::stoi(l), std::stoi(r)});
+        assert(InBounds(bytes.back()));
+    }
+
+    NestedVector<2, bool> occupied = ConstVector(false, kSizeY, kSizeX);
+    for (int i = 0; i < bytes.size(); i++) {
+        occupied[bytes[i].y][bytes[i].x] = true;
+
+        NestedVector<2, int> d = ConstVector(kInf, kSizeY, kSizeX);
+        std::queue<Coord> q;
+        d[0][0] = 0;
+        q.push({0, 0});
+        while (!q.empty()) {
+            Coord u = q.front();
+            q.pop();
+            for (int dir = 0; dir < 4; dir++) {
+                Coord next = {u.x + kDirs[dir].x, u.y + kDirs[dir].y};
+                if (!InBounds(next) || occupied[next.y][next.x]) {
+                    continue;
+                }
+                if (d[next.y][next.x] == kInf) {
+                    d[next.y][next.x] = d[u.y][u.x] + 1;
+                    q.push(next);
+                }
+            }
+        }
+
+        if (d[kSizeY - 1][kSizeX - 1] == kInf) {
+            std::cout << bytes[i].x << "," << bytes[i].y << std::endl;
+            break;
+        }
+    }
+
+    return 0;
+}
