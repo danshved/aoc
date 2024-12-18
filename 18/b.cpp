22c1a1b43bd5d4c41c37b498a77e04699686e507
Dan Shved <danshved@gmail.com>
2024-12-18 21:01:28 +0100

    Improve style

--- 18/b.cpp
+++ 18/b.cpp
@@ -19,15 +19,15 @@
 #include "order.h"
 #include "parse.h"
 
-const int kSizeX = 71;
-const int kSizeY = 71;
-
 struct Coord {
     int x, y;
 };
 
 const Coord kDirs[4] = {{1, 0}, {0, -1}, {-1, 0}, {0, 1}};
 const int kInf = std::numeric_limits<int>::max();
+
+const int kSizeX = 71;
+const int kSizeY = 71;
 
 bool InBounds(const Coord& c) {
     return c.x >= 0 && c.x < kSizeX && c.y >= 0 && c.y < kSizeY;
@@ -42,12 +42,12 @@ int main() {
         assert(InBounds(bytes.back()));
     }
 
-    NestedVector<2, bool> occupied = ConstVector(false, kSizeY, kSizeX);
+    NestedVector<2, bool> occupied = ConstVector(false, kSizeX, kSizeY);
     int i;
     for (i = 0; i < bytes.size(); i++) {
-        occupied[bytes[i].y][bytes[i].x] = true;
+        occupied[bytes[i].x][bytes[i].y] = true;
 
-        NestedVector<2, int> d = ConstVector(kInf, kSizeY, kSizeX);
+        NestedVector<2, int> d = ConstVector(kInf, kSizeX, kSizeY);
         std::queue<Coord> q;
         d[0][0] = 0;
         q.push({0, 0});
@@ -56,17 +56,17 @@ int main() {
             q.pop();
             for (int dir = 0; dir < 4; dir++) {
                 Coord v = {u.x + kDirs[dir].x, u.y + kDirs[dir].y};
-                if (!InBounds(v) || occupied[v.y][v.x]) {
+                if (!InBounds(v) || occupied[v.x][v.y]) {
                     continue;
                 }
-                if (d[v.y][v.x] == kInf) {
-                    d[v.y][v.x] = d[u.y][u.x] + 1;
+                if (d[v.x][v.y] == kInf) {
+                    d[v.x][v.y] = d[u.x][u.y] + 1;
                     q.push(v);
                 }
             }
         }
 
-        if (d[kSizeY - 1][kSizeX - 1] == kInf) {
+        if (d[kSizeX - 1][kSizeY - 1] == kInf) {
             break;
         }
     }
