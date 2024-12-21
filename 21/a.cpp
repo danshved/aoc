52ee6bead08161c465dedf2a2f8f6e3109166a59
Dan Shved <danshved@gmail.com>
2024-12-21 09:38:49 +0100

    Add solutions for day 21

---
+++ 21/a.cpp
@@ -0,0 +1,112 @@
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <limits>
+#include <map>
+#include <optional>
+#include <queue>
+#include <ranges>
+#include <set>
+#include <string>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+#include "collections.h"
+#include "numbers.h"
+#include "order.h"
+#include "parse.h"
+
+struct Coord {
+    int i;
+    int j;
+
+    bool operator==(const Coord&) const = default;
+};
+
+using Keypad = std::unordered_map<char, Coord>;
+
+const Keypad kNumbers = {
+    {'7', {0, 0}},
+    {'8', {0, 1}},
+    {'9', {0, 2}},
+    {'4', {1, 0}},
+    {'5', {1, 1}},
+    {'6', {1, 2}},
+    {'1', {2, 0}},
+    {'2', {2, 1}},
+    {'3', {2, 2}},
+    {'x', {3, 0}},
+    {'0', {3, 1}},
+    {'A', {3, 2}},
+};
+
+const Keypad kArrows = {
+    {'^', {0, 1}},
+    {'A', {0, 2}},
+    {'<', {1, 0}},
+    {'x', {0, 0}},
+    {'v', {1, 1}},
+    {'>', {1, 2}},
+};
+
+std::vector<std::string> GetVariants(const std::string& s, int i, const Keypad& kp, Coord pos) {
+    if (i == s.size()) {
+        return {""};
+    }
+
+    Coord next = kp.at(s[i]);
+    std::vector<std::string> from_next = GetVariants(s, i + 1, kp, next);
+
+    std::string i_moves = (next.i > pos.i)   ? std::string(next.i - pos.i, 'v')
+                          : (next.i < pos.i) ? std::string(pos.i - next.i, '^')
+                                              : std::string();
+    std::string j_moves = (next.j > pos.j)   ? std::string(next.j - pos.j, '>')
+                          : (next.j < pos.j) ? std::string(pos.j - next.j, '<')
+                                              : std::string();
+    Coord forbidden = kp.at('x');
+    std::vector<std::string> result;
+
+    if (Coord{pos.i, next.j} != forbidden) {
+        std::string prefix = j_moves + i_moves + "A";
+        for (const std::string& rest : from_next) {
+            result.push_back(prefix + rest);
+        }
+    }
+
+    if (next.i != pos.i && next.j != pos.j && Coord{next.i, pos.j} != forbidden) {
+        std::string prefix = i_moves + j_moves + "A";
+        for (const std::string& rest : from_next) {
+            result.push_back(prefix + rest);
+        }
+    }
+
+    return result;
+}
+
+std::vector<std::string> Get(const std::vector<std::string>& ss, const Keypad& kp) {
+    return ConcatMap(ss, [&](const std::string& s) {
+        return GetVariants(s, 0, kp, kp.at('A'));
+    });
+}
+
+int main() {
+    std::vector<std::string> lines = Split(Trim(GetContents("input.txt")), '\n');
+
+    int answer = 0;
+    for (const std::string& s : lines) {
+        std::vector<std::string> vars = Get(Get(Get({s}, kNumbers), kArrows), kArrows);
+        
+        int best = std::numeric_limits<int>::max();
+        for (const std::string& var : vars) {
+            best = std::min(best, (int)var.size());
+        }
+
+        answer += std::stoi(s) * best;
+    }
+
+    std::cout << answer << std::endl;
+    return 0;
+}
---
+++ 21/b.cpp
@@ -0,0 +1,114 @@
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <limits>
+#include <map>
+#include <optional>
+#include <queue>
+#include <ranges>
+#include <set>
+#include <string>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+#include "collections.h"
+#include "numbers.h"
+#include "order.h"
+#include "parse.h"
+
+struct Coord {
+    int i;
+    int j;
+
+    bool operator==(const Coord&) const = default;
+};
+
+using Keypad = std::unordered_map<char, Coord>;
+
+const Keypad kNumbers = {
+    {'7', {0, 0}},
+    {'8', {0, 1}},
+    {'9', {0, 2}},
+    {'4', {1, 0}},
+    {'5', {1, 1}},
+    {'6', {1, 2}},
+    {'1', {2, 0}},
+    {'2', {2, 1}},
+    {'3', {2, 2}},
+    {'x', {3, 0}},
+    {'0', {3, 1}},
+    {'A', {3, 2}},
+};
+
+const Keypad kArrows = {
+    {'^', {0, 1}},
+    {'A', {0, 2}},
+    {'<', {1, 0}},
+    {'x', {0, 0}},
+    {'v', {1, 1}},
+    {'>', {1, 2}},
+};
+
+long long CountMoves(Coord start, Coord end, const Keypad& kp, int below);
+
+// How many human keypresses it costs to push a given sequence of keys on a
+// given keypad, assuming that the pointer on this keypad and the pointers on
+// all keypads upstream start on 'A' and finish on 'A'.
+long long StrCost(const std::string& s, const Keypad& kp, int robots) {
+    Coord pos = kp.at('A');
+    long long result = 0;
+    for (char c : s) {
+        Coord next = kp.at(c);
+        result += CountMoves(pos, next, kp, robots) + 1;
+        pos = next;
+    }
+    result += CountMoves(pos, kp.at('A'), kp, robots);
+    return result;
+}
+
+// How many human keypresses it costs to move the pointer from 'start'
+// to 'end', assuming all pointers upstream start on 'A' and finish on 'A'.
+long long CountMoves(Coord start, Coord end, const Keypad& kp, int robots) {
+    static NestedVector<5, long long> d = ConstVector(-1ll, 4, 3, 4, 3, 27);
+    long long& result = d[start.i][start.j][end.i][end.j][robots];
+    if (result != -1) {
+        return result;
+    }
+
+    if (robots == 0) {
+        // The pointer is the human finger, it costs nothing to move it.
+        return result = 0;
+    }
+
+    // The pointer is a robotic arm. Try to move it in two ways:
+    // horizontal-then-vertical and vertical-then-horizontal.
+    std::string i_moves = (end.i > start.i)   ? std::string(end.i - start.i, 'v')
+                          : (end.i < start.i) ? std::string(start.i - end.i, '^')
+                                              : std::string();
+    std::string j_moves = (end.j > start.j)   ? std::string(end.j - start.j, '>')
+                          : (end.j < start.j) ? std::string(start.j - end.j, '<')
+                                              : std::string();
+    Coord forbidden = kp.at('x');
+
+    result = std::numeric_limits<long long>::max();
+    if (Coord{start.i, end.j} != forbidden) {
+        result = std::min(result, StrCost(j_moves + i_moves, kArrows, robots - 1));
+    }
+    if (Coord{end.i, start.j} != forbidden) {
+        result = std::min(result, StrCost(i_moves + j_moves, kArrows, robots - 1));
+    }
+    return result;
+}
+
+int main() {
+    std::vector<std::string> lines = Split(Trim(GetContents("input.txt")), '\n');
+    long long answer = 0;
+    for (const std::string& s : lines) {
+        answer += std::stoll(s) * StrCost(s, kNumbers, 26);
+    }
+    std::cout << answer << std::endl;
+    return 0;
+}
