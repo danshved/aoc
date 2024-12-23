a379dd8a5a95d31e1bade658e47018c2c07515dc
Dan Shved <danshved@gmail.com>
2024-12-23 12:29:29 +0100

    Improve readability for DFS()

--- 5/graph_search.h
+++ 5/graph_search.h
@@ -3,47 +3,51 @@
 
 #include <vector>
 
-enum class DFSLook { kVisited, kSkippedBack, kSkippedOld };
+enum class DFSColor {
+    kNew = 0,
+    kOnPath = 1,
+    kOld = 2,
+};
 
-template <typename Node, typename VisitFunc>
+template <typename Node, typename Hasher, typename VisitFunc>
 class DFSState {
    public:
     DFSState(VisitFunc& visit) : visit_(visit) {}
 
-    DFSLook Look(const Node& node) {
-        switch (colors_[node]) {
-        case 0:
-            colors_[node] = 1;
+    // Tell the search to look at `node`. This means that logically there is
+    // an edge going from the current node to `node`.
+    //
+    // Returns the DFS color of `node` at the time when Look() was called.
+    // If this is `kOld` or `kOnPath`, Look() has been a no-op.
+    // If this is kNew, Look() has followed the edge into `node` recursively.
+    DFSColor Look(const Node& node) {
+        DFSColor& color = colors_[node];
+        if (color == DFSColor::kNew) {
+            color = DFSColor::kOnPath;
             path_.push_back(node);
             visit_(*this, node);
             path_.pop_back();
-            colors_[node] = 2;
-            return DFSLook::kVisited;
-        case 1:
-            return DFSLook::kSkippedBack;
-        case 2:
-            return DFSLook::kSkippedOld;
-        default:
-            assert(false);
+            colors_[node] = DFSColor::kOld;
+            return DFSColor::kNew;
         }
+        return color;
     }
 
     const std::vector<Node>& Path() {
         return path_;
     }
-    
+
    private:
     VisitFunc& visit_;
-    std::unordered_map<Node, int> colors_;
+    std::unordered_map<Node, DFSColor, Hasher> colors_;
     std::vector<Node> path_;
 };
 
-template <typename Node, typename StartFunc, typename VisitFunc>
+template <typename Node, typename Hasher = std::hash<Node>,
+          typename StartFunc, typename VisitFunc>
 void DFS(StartFunc&& start, VisitFunc&& visit) {
-    DFSState<Node, VisitFunc> state(visit);
+    DFSState<Node, Hasher, VisitFunc> state(visit);
     start(state);
 }
-
-// TODO: add support for custom hashers.
 
 #endif
