a28ad5792e2522747ccd6a81335d4b1ae3fb8299
Dan Shved <danshved@gmail.com>
2024-12-24 23:58:55 +0100

    Use grid.h in day 8

--- 8/a.cpp
+++ 8/a.cpp
@@ -1,60 +1,43 @@
+#include <algorithm>
 #include <iostream>
-#include <string>
-#include <vector>
+#include <limits>
 #include <map>
+#include <optional>
 #include <set>
+#include <string>
+#include <tuple>
 #include <unordered_map>
 #include <unordered_set>
-#include <tuple>
-#include <optional>
-#include <algorithm>
 #include <utility>
-#include <limits>
+#include <vector>
 
-#include "parse.h"
+#include "collections.h"
+#include "grid.h"
 #include "lex.h"
+#include "parse.h"
 
 int main() {
     std::vector<std::string> input = Split(Trim(GetContents("input.txt")), '\n');
-    int height = input.size();
-    int width = input[0].size();
+    auto [size_i, size_j] = Sizes<2>(input);
 
-    std::vector<std::vector<bool>> antinode(height, std::vector<bool>(width, false));
-    for (int i1 = 0; i1 < height; i1++) {
-        for (int j1 = 0; j1 < width; j1++) {
-            if (input[i1][j1] == '.') {
+    std::unordered_set<Coord> antinodes;
+    for (Coord a : Bounds(size_i, size_j)) {
+        if (input[a.i][a.j] == '.') {
+            continue;
+        }
+        for (Coord b : Bounds(size_i, size_j)) {
+            if (a == b || input[a.i][a.j] != input[b.i][b.j]) {
                 continue;
             }
-            for (int i2 = 0; i2 < height; i2++) {
-                for (int j2 = 0; j2 < width; j2++) {
-                    if (input[i2][j2] != input[i1][j1]) {
-                        continue;
-                    }
-                    if (i1 == i2 && j1 == j2) {
-                        continue;
-                    }
-
-                    int i3 = 2 * i2 - i1;
-                    int j3 = 2 * j2 - j1;
-                    if (i3 >= 0 && i3 < height && j3 >= 0 && j3 < width) {
-                        antinode[i3][j3] = true;
-                    }
-
-                    i3 = 2 * i1 - i2;
-                    j3 = 2 * j1 - j2;
-                    if (i3 >= 0 && i3 < height && j3 >= 0 && j3 < width) {
-                        antinode[i3][j3] = true;
-                    }
-                }
+            if (InBounds(2 * a - b, size_i, size_j)) {
+                antinodes.insert(2 * a - b);
+            }
+            if (InBounds(2 * b - a, size_i, size_j)) {
+                antinodes.insert(2 * b - a);
             }
         }
     }
 
-    int answer = 0;
-    for (const std::vector<bool>& v : antinode) {
-        answer += std::count(v.begin(), v.end(), true);
-    }
-
-    std::cout << answer << std::endl;
+    std::cout << antinodes.size() << std::endl;
     return 0;
 }
--- 8/b.cpp
+++ 8/b.cpp
@@ -1,84 +1,49 @@
+#include <algorithm>
 #include <iostream>
-#include <string>
-#include <vector>
+#include <limits>
 #include <map>
+#include <optional>
 #include <set>
+#include <string>
+#include <tuple>
 #include <unordered_map>
 #include <unordered_set>
-#include <tuple>
-#include <optional>
-#include <algorithm>
 #include <utility>
-#include <limits>
+#include <vector>
 
-#include "parse.h"
+#include "collections.h"
+#include "grid.h"
 #include "lex.h"
-
-int gcd(int a, int b) {
-    if (a < 0) a = -a;
-    if (b < 0) b = -b;
-    while (b != 0) {
-        a %= b;
-        std::swap(a, b);
-    }
-    return a;
-}
+#include "numbers.h"
+#include "parse.h"
 
 int main() {
     std::vector<std::string> input = Split(Trim(GetContents("input.txt")), '\n');
-    int height = input.size();
-    int width = input[0].size();
+    auto [size_i, size_j] = Sizes<2>(input);
 
-    std::vector<std::vector<bool>> antinode(height, std::vector<bool>(width, false));
-    for (int i1 = 0; i1 < height; i1++) {
-        for (int j1 = 0; j1 < width; j1++) {
-            if (input[i1][j1] == '.') {
+    std::unordered_set<Coord> antinodes;
+    for (Coord a : Bounds(size_i, size_j)) {
+        if (input[a.i][a.j] == '.') {
+            continue;
+        }
+        for (Coord b : Bounds(size_i, size_j)) {
+            if (a == b || input[a.i][a.j] != input[b.i][b.j]) {
                 continue;
             }
-            for (int i2 = 0; i2 < height; i2++) {
-                for (int j2 = 0; j2 < width; j2++) {
-                    if (input[i2][j2] != input[i1][j1]) {
-                        continue;
-                    }
-                    if (i1 == i2 && j1 == j2) {
-                        continue;
-                    }
 
-                    int di = i1 - i2;
-                    int dj = j1 - j2;
-                    int t = gcd(di, dj);
-                    di /= t;
-                    dj /= t;
+            Coord delta = b - a;
+            int d = Gcd(delta.i, delta.j);
+            delta = Coord{delta.i / d, delta.j / d};
 
-                    for (int t = 0; ; t++) {
-                        int i3 = i1 + t * di;
-                        int j3 = j1 + t * dj;
-                        if (i3 >= 0 && i3 < height && j3 >= 0 && j3 < width) {
-                            antinode[i3][j3] = true;
-                        } else {
-                            break;
-                        }
-                    }
-
-                    for (int t = 0; ; t++) {
-                        int i3 = i1 - t * di;
-                        int j3 = j1 - t * dj;
-                        if (i3 >= 0 && i3 < height && j3 >= 0 && j3 < width) {
-                            antinode[i3][j3] = true;
-                        } else {
-                            break;
-                        }
-                    }
-                }
+            for (Coord cur = a; InBounds(cur, size_i, size_j); cur += delta) {
+                antinodes.insert(cur);
+            }
+            for (Coord cur = a; InBounds(cur, size_i, size_j); cur -= delta) {
+                antinodes.insert(cur);
             }
         }
     }
 
-    int answer = 0;
-    for (const std::vector<bool>& v : antinode) {
-        answer += std::count(v.begin(), v.end(), true);
-    }
-
-    std::cout << answer << std::endl;
+    std::cout << antinodes.size() << std::endl;
     return 0;
 }
