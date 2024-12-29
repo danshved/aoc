a6638b9c5c98b4d83513e1ef1b20e2f49961536d
Dan Shved <danshved@gmail.com>
2024-12-29 21:42:06 +0100

    Update day 19

--- 19/b.cpp
+++ 19/b.cpp
@@ -1,28 +1,13 @@
-#include <algorithm>
-#include <cmath>
 #include <iostream>
-#include <limits>
-#include <map>
-#include <optional>
-#include <queue>
-#include <ranges>
-#include <set>
 #include <string>
-#include <tuple>
-#include <unordered_map>
-#include <unordered_set>
-#include <utility>
 #include <vector>
 
 #include "collections.h"
-#include "numbers.h"
-#include "order.h"
 #include "parse.h"
 
 int main() {
-    std::vector<std::string> lines = Split(Trim(GetContents("input.txt")), '\n');
-    auto [top, queries] = Split2(lines, std::string());
-    std::vector<std::string> pieces = Split(top[0], ',');
+    auto [top, queries] = Split2(Split(Trim(GetContents("input.txt")), "\n"), {""});
+    std::vector<std::string> pieces = Split(top[0], ",");
     for (std::string& s : pieces) {
         s = Trim(s);
     }
