a69440a6c81909774451934e0d36cacdad1e8291
Dan Shved <danshved@gmail.com>
2024-12-29 14:34:08 +0100

    Golf day 4 part 1

--- 4/a.cpp
+++ 4/a.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
+#include <ranges>
 #include <string>
 #include <vector>
-#include <ranges>
 
 #include "collections.h"
 #include "grid.h"
@@ -12,17 +12,14 @@ const char kXmas[] = "XMAS";
 int main() {
     std::vector<std::string> input = Split(Trim(GetContents("input.txt")), "\n");
     auto [size_i, size_j] = Sizes<2>(input);
-    auto is_xmas = [&](Coord start, Coord dir) {
-        return std::ranges::all_of(std::ranges::iota_view(0, 4), [&](int k) {
-            Coord c = start + k * dir;
-            return (InBounds(c, size_i, size_j) && input[c.i][c.j] == kXmas[k]);
-        });
-    };
 
     int answer = 0;
     for (Coord start : Bounds(size_i, size_j)) {
         answer += std::ranges::count_if(Adj8({0, 0}), [&](Coord dir) {
-            return is_xmas(start, dir);
+            return std::ranges::all_of(std::ranges::iota_view(0, 4), [&](int k) {
+                Coord c = start + k * dir;
+                return InBounds(c, size_i, size_j) && input[c.i][c.j] == kXmas[k];
+            });
         });
     }
 
