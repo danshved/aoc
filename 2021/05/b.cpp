80735f9b84b43a75ac8f533add480182409f1269
Dan Shved <danshved@gmail.com>
2024-12-31 00:17:58 +0100

    Solve 2021/05

---
+++ 2021/05/b.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <ranges>
+#include <string>
+#include <unordered_map>
+
+#include "grid.h"
+#include "parse.h"
+
+int main() {
+    std::unordered_map<Coord, int> counts;
+    for (const std::string& line : Split(Trim(GetContents("input.txt")), "\n")) {
+        auto [a, b, c, d] = SplitN(line, ",", " -> ", ",");
+        Coord from = {std::stoi(a), std::stoi(b)};
+        Coord to = {std::stoi(c), std::stoi(d)};
+        for (Coord c : PathCC(from, to)) {
+            counts[c]++;
+        }
+    }
+
+    int answer = std::ranges::count_if(counts | std::views::values, [](int count) {
+        return count >= 2;
+    });
+    std::cout << answer << std::endl;
+    return 0;
+}
